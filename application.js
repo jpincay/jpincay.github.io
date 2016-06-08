c3ae8bd389c2eddf7fc2d6773a8b40d69685b6d6
jpincay <jpincay@kippnyccp.org>
2016-06-08 12:32:30 -0400

    Update application.js

--- application.js
+++ application.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
   $("#submit").click(function(){
     var first = $("#info_1").val();
     var response= "New Info:" + first + "";
-    $("#my_list").append("<li>" + response + "</li>");
+    $("#cool").append("<p>" + response + "</p>");
 });
