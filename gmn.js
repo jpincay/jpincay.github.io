0d799980302c23bcae375b0cffbfb54c148f39ab
jpincay <jpincay@kippnyccp.org>
2016-06-15 21:36:25 -0400

    Update gmn.js

--- gmn.js
+++ gmn.js
@@ -1,13 +1,16 @@
-$(document).ready(function(){
-
-    var random_num = Math.floor((Math.random()*100) + 1);
-
-    //create click event for submit button
-
-
-
-
-    //create click event for clear button
-
-
-});
+$(document).ready(function() {
+      var random_num = Math.floor((Math.random() * 100) + 1);
+      $("#submit").click(function() {
+            var guess =
+              $("#guess").val();
+            if (guess == random_num) {
+              alert("You Got It!!")
+              $("#response").html("<p>You Got It!!</p>");
+            } else if (guess < random_num) {
+                alert("You're close. Guess higher.")
+                $("#response").html("<p>You're close. Guess higher.</p>");
+            } else {
+                  $("#response").html("<p>You're close. Guess higher.</p>");
+            }
+      });
+})
